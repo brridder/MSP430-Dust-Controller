39be46eaf21337472a94e9363755d8fa482457c9
Ben Ridder <ben@ridder.ca>
2013-05-23 17:50:44 -0400

    cleaned up the code a bit

--- dust_controller.c
+++ dust_controller.c
@@ -12,30 +12,35 @@
 #define BUTTON1             BIT6
 #define BUTTON2             BIT7
 
-#define MAX_ON_TIME         30
-#define MIN_ON_TIME         10
+#define MAX_ON_TIME         ON_TIME_INTERVAL*3
+#define MIN_ON_TIME         ON_TIME_INTERVAL*1
 #define ON_TIME_INTERVAL    10
 
 #define MAX_OFF_TIME        OFF_TIME_INTERVAL*3
 #define MIN_OFF_TIME        OFF_TIME_INTERVAL*1
-#define OFF_TIME_INTERVAL   10//3600
+#define OFF_TIME_INTERVAL   3600
 
 #define STATE_RELAY_OFF     1
 #define STATE_RELAY_ON      2
 
+#define CLOCK_SPEED_HZ      32765
+
+// Debounce flags
 char button_1_flag = 0;
 char button_2_flag = 0;
+
+//  Counters
 char one_second_timer_counter = 0;
-char current_state = STATE_RELAY_OFF;
-
-int off_time = MIN_OFF_TIME;
-int on_time = MIN_ON_TIME;
-
 int off_time_flash_counter = 0;
 int on_time_flash_counter = 0;
-
 int main_time_counter = 0;
 
+// State
+char current_state = STATE_RELAY_OFF;
+
+// Time periods
+int off_time = MIN_OFF_TIME;
+int on_time = MIN_ON_TIME;
 
 void initLEDs(void) {
     P1DIR |= (LED0 + LED1 + LED2); // Set the LED pins as outputs
@@ -43,8 +48,7 @@ void initLEDs(void) {
 }
 
 void initOffboardOutputs() {
-    P1DIR |= (OUT0 + OUT1 + OUT2);
-    // TODO
+    P1DIR |= (OUT0 + OUT1 + OUT2); // Set the three other out pins to outputs
 }
 
 void initButtons(void) {
@@ -55,20 +59,23 @@ void initButtons(void) {
 
 void initTimer(void) {
     TACTL = TASSEL_1 | MC_1; // Use ACLK, up to CCR0
-    TACCTL0 = CCIE;
-    TA0CCR0 = 32765/10; // Approximately 100 milliseconds using the external clk
+    TACCTL0 = CCIE; // Enable counter interrupts
+    TA0CCR0 = CLOCK_SPEED_HZ/10; // Approximately 100 milliseconds using the external clk
 }
 
+// Turn on the relay LED and the relay output.
 void relayOn(void) {
-    P1OUT |= (LED0);
+    P1OUT |= LED0; 
     P1OUT |= OUT0;
 }
 
+// Turn off the relay LED and relay output
 void relayOff(void) {
     P1OUT &= ~(LED0);
     P1OUT &= ~(OUT0);
 }
 
+// State machine for the relay.
 void checkState(void) {
     if (current_state == STATE_RELAY_OFF) {
         if (main_time_counter >= off_time) {
@@ -88,34 +95,35 @@ void checkState(void) {
 
 int main(void) {
     WDTCTL = WDTPW + WDTHOLD; // Stop WDT
-
+    
+    // Init all the things.
     initLEDs();
     initButtons();
     initTimer();
     initOffboardOutputs();
 
-    main_time_counter = 0;
-
-    WRITE_SR(GIE);
+    WRITE_SR(GIE); // Enable global interrupts
 
     while(1) {
+        // Just loop over the state machine.
         checkState();
     }
 }
 
+// ISR for the buttons.
 interrupt(PORT1_VECTOR) port1_isr(void) {
-    if ((P1IFG & BUTTON1) == BUTTON1) {
-        if (button_1_flag == 0) {
-            button_1_flag++;
+    if ((P1IFG & BUTTON1) == BUTTON1) { // Button for hour off periods
+        if (button_1_flag == 0) { // Debounce
+            button_1_flag = 1;
             off_time += OFF_TIME_INTERVAL;
             if (off_time > MAX_OFF_TIME) {
                 off_time = MIN_OFF_TIME;
             }
         }
         P1IFG &= ~BUTTON1;
-    } else if ((P1IFG & BUTTON2) == BUTTON2) {
-        if (button_2_flag == 0) {
-            button_2_flag++;
+    } else if ((P1IFG & BUTTON2) == BUTTON2) { // Button for seconds on periods
+        if (button_2_flag == 0) { // Debounce
+            button_2_flag = 1;
             on_time += ON_TIME_INTERVAL;
             if (on_time > MAX_ON_TIME) {
                 on_time = MIN_ON_TIME;
@@ -125,24 +133,36 @@ interrupt(PORT1_VECTOR) port1_isr(void) {
     }
 }
 
+// ISR for timer0 A0
 interrupt(TIMER0_A0_VECTOR) timer_a0_isr(void) {
     one_second_timer_counter++;
     on_time_flash_counter += ON_TIME_INTERVAL/2;
     off_time_flash_counter += OFF_TIME_INTERVAL/2;
+
+    // Check to see if one second has elapsed
     if (one_second_timer_counter >= 10) {
-        one_second_timer_counter = 0;
+        // Reset the LEDs
         P1OUT &= ~(LED1);
         P1OUT &= ~(LED2);
+
+        // Reset the local counters
+        one_second_timer_counter = 0;
         on_time_flash_counter = 0;
         off_time_flash_counter = 0;
+
+        // Increment the total second counter
         main_time_counter++;
     }
+
     if (on_time_flash_counter < on_time) {
         P1OUT ^= (LED1);
     }
+
     if (off_time_flash_counter < off_time) {
         P1OUT ^= (LED2);
     }
+    
+    // Debounce flags for the buttons
     button_1_flag = 0;
     button_2_flag = 0;
 }
